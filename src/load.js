ca86a6d457d58f5af9426850654f7a5cdcdfe33a
olga.grigorieva <olga.grigorieva@socinform.ru>
2016-09-03 16:44:38 +0300

    Постраничный вывод и делегирование событий

--- src/load.js
+++ src/load.js
@@ -1,9 +1,18 @@
 'use strict';
 
-module.exports = function(url, callback) {
-  window.callback = callback;
+module.exports = function(url, params, callback) {
 
-  var scriptEl = document.createElement('script');
-  scriptEl.src = url + '?callback=callback';
-  document.body.appendChild(scriptEl);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url +
+      '?from=' + (params.from || 0) +
+      '&to=' + (params.to || Infinity) +
+      '&filter=' + (params.filter || 'default')
+    );
+  xhr.onload = function(evt) {
+    var requestObj = evt.target;
+    var response = requestObj.response;
+    callback(JSON.parse(response));
+  };
+
+  xhr.send();
 };
